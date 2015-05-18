ab195b5efbf1806725c3e704569303897ef7972f
Koryakin Pavel <koryakin.pavel88@gmail.com>
2015-05-17 23:25:31 -0400

    initial commit

---
+++ PresentationLayer/Controllers/PatientController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace PresentationLayer.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult Index()
+        {
+            HospitalEntities db = new HospitalEntities();
+            List<Patient> pList = db.Patients.ToList();
+            return View(pList);
+        }
+
+        // GET: Patient/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(new HospitalEntities().Patients.First(q => q.PatientId == id));
+        }
+
+        // GET: Patient/Delete/5
+        public ActionResult Delete(int id)
+        {
+            HospitalEntities db = new HospitalEntities();
+            Patient p = db.Patients.Where(q => q.PatientId.Equals(id)).FirstOrDefault();
+            if (p != null)
+            {
+                db.Patients.Remove(p);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
